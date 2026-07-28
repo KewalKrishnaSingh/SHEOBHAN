7f006792b5a7cd22d9cfa091c7e62dcc5d317be4
KEWAL KRISHNA SINGH <100328473+KewalKrishnaSingh@users.noreply.github.com>
2026-07-28 12:51:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector(".menu-toggle"),nav=document.querySelector(".nav");toggle.addEventListener("click",()=>{nav.classList.toggle("open");toggle.setAttribute("aria-expanded",nav.classList.contains("open"))});document.querySelectorAll(".nav a").forEach(a=>a.addEventListener("click",()=>nav.classList.remove("open")));
